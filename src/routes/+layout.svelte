5706b0e21d88b48ee3f2456f509313414604342c
pyephebes0 <xyz928.pyephebes@gmail.com>
2025-04-26 17:15:03 +0700

    แก้ไข layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,7 +27,7 @@
     }}>Logout</a>
   {:else}
     <a href="/login">Login</a>
-    <a href="/register">Register</a>
+    <a href="/register">ลงทะเบียน</a>
   {/if}
 </nav>
 <hr class="nav-divider" />
